592699a9585055a91ddca1c83fc4c7d952ae8dc9
Vasily Kulagin <vascool66@gmail.com>
2025-12-20 17:43:23 +0200

    перенос агента и сервера

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"sys-metrics/internal/handler/router"
+	"sys-metrics/pkg/server"
+)
+
 func main() {
+	e := server.NewServer(server.DefaultHost, server.DefaultPort, router.GetRouter())
+	if e != nil {
+		panic(e)
+	}
 }
