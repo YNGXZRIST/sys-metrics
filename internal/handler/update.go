a6f0c03dd061dc5672f7e0b348620782de125afd
Vasily Kulagin <vascool66@gmail.com>
2026-01-28 22:41:04 +0200

    добавление апи ендпоинта для /update

--- internal/handler/update.go
+++ internal/handler/update.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
 	collector "sys-metrics/internal/agent"
@@ -16,9 +18,42 @@ func UpdateHandler(w http.ResponseWriter, r *http.Request) {
 	name = collector.GetMetricType(name)
 	err := serviceMetrics.Update(metricType, name, value)
 	if err != nil {
-
 		responsewriter.WriteBadRequest(w)
 		return
 	}
 	responsewriter.WriteSuccess(w)
+}
+
+type RequestBody struct {
+	Id    string `json:"id"`
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+func UpdateHandlerJSON(w http.ResponseWriter, r *http.Request) {
+	var req RequestBody
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(&req); err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err := serviceMetrics.Update(req.Type, req.Id, req.Value)
+	if err != nil {
+		responsewriter.WriteBadRequest(w)
+		return
+	}
+	metric, err := getMetricFromStorage(req.Type, req.Id)
+	if err != nil {
+		responsewriter.WriteServerError(w)
+		return
+	}
+	responsewriter.WriteSuccessStatus(w)
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(metric); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 }
--- internal/handler/update_test.go
+++ internal/handler/update_test.go
@@ -1,10 +1,13 @@
 package handler
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"sys-metrics/internal/common"
 	"sys-metrics/internal/model/metrics"
 	svc "sys-metrics/internal/service/metrics"
@@ -68,6 +71,98 @@ func TestUpdateHandler(t *testing.T) {
 			if res.StatusCode != tt.want {
 				t.Errorf("UpdateHandler() = %v, want %v", res.StatusCode, tt.want)
 			}
+		})
+	}
+}
+
+func TestUpdateHandlerJSON(t *testing.T) {
+	type args struct {
+		ID    string `json:"id"`
+		Type  string `json:"type"`
+		Value string `json:"value"`
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+	}{
+		{
+			name: "success gauge",
+			args: args{
+				ID:    "sys-metrics",
+				Type:  common.Gauge,
+				Value: "123.45",
+			},
+			wantStatus: http.StatusOK,
+		},
+		{
+			name: "success counter",
+			args: args{
+				ID:    "sys-metrics",
+				Type:  common.Counter,
+				Value: "100",
+			},
+			wantStatus: http.StatusOK,
+		},
+		{
+			name: "unknown type",
+			args: args{
+				ID:    "sys-metrics",
+				Type:  "test",
+				Value: "100",
+			},
+			wantStatus: http.StatusBadRequest,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			counters := memstorage.NewMemStorage[string, *metrics.Counter]()
+			gauges := memstorage.NewMemStorage[string, *metrics.Gauge]()
+			svc.Init(counters, gauges)
+			payload, err := json.Marshal(tt.args)
+			if err != nil {
+				t.Fatalf("failed to marshal request body: %v", err)
+			}
+			reqBody := bytes.NewReader(payload)
+			req := httptest.NewRequest(http.MethodPost, "/value", reqBody)
+			w := httptest.NewRecorder()
+			UpdateHandlerJSON(w, req)
+			res := w.Result()
+			res.Body.Close()
+			defer func(Body io.ReadCloser) {
+				err := Body.Close()
+				if err != nil {
+					fmt.Println(err)
+				}
+			}(res.Body)
+
+			if res.StatusCode != tt.wantStatus {
+				t.Errorf("UpdateHandlerJSON() status = %v, want %v", res.StatusCode, http.StatusOK)
+			}
+			if tt.args.Type == common.Gauge {
+				gauge, err := gauges.Get(tt.args.ID)
+				if err != nil {
+					t.Fatalf("gauges.Get(%s): expected %v, got %v", tt.args.ID, nil, err)
+				}
+				expectedValue, err := strconv.ParseFloat(tt.args.Value, 64)
+				if err != nil {
+					t.Fatalf("strconv.ParseFloat(%s): expected %v, got %v", tt.args.Value, nil, err)
+				}
+				if *gauge.Value != expectedValue {
+					t.Errorf("gauge value = %v, want %v", *gauge.Value, expectedValue)
+				}
+			}
+			if tt.args.Type == common.Counter {
+				counter, err := counters.Get(tt.args.ID)
+				if err != nil {
+					t.Fatalf("counters.Get(%s): expected %v, got %v", tt.args.ID, nil, err)
+				}
+				expectedValue := tt.args.Value
+				if fmt.Sprintf("%d", *counter.Delta) != expectedValue {
+					t.Errorf("counter value = %v, want %v", *counter.Delta, expectedValue)
+				}
+			}
+
 		})
 	}
 }
