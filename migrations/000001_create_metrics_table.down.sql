8d4975ab1c7ce73e9795f80e5ad98817abe566c7
Vasily Kulagin <vascool66@gmail.com>
2026-02-14 19:02:44 +0200

    добавлен id для получения метрик чанками

--- migrations/000001_create_metrics_table.down.sql
+++ migrations/000001_create_metrics_table.down.sql
@@ -1,3 +1,3 @@
 DROP INDEX IF EXISTS idx_metrics_mtype;
-DROP INDEX IF EXISTS idx_metrics_id;
+DROP INDEX IF EXISTS idx_metrics_name;
 DROP TABLE IF EXISTS metrics;
--- migrations/000001_create_metrics_table.up.sql
+++ migrations/000001_create_metrics_table.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS metrics (
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
     mtype VARCHAR(50) NOT NULL CHECK (mtype IN ('counter', 'gauge')),
     delta BIGINT,
     value DOUBLE PRECISION,
     hash VARCHAR(255)
 );
 
-CREATE INDEX IF NOT EXISTS idx_metrics_id ON metrics(id);
+CREATE INDEX IF NOT EXISTS idx_metrics_name ON metrics(name);
 CREATE INDEX IF NOT EXISTS idx_metrics_mtype ON metrics(mtype);
